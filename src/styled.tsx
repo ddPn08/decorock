e8cd10aaaefc7bd7c7c92c9642d229e92936b934
ddPn08 <pyn.g.1125@gmail.com>
2022-12-27 00:29:06 +0900

    fix reactivity

--- src/styled.tsx
+++ src/styled.tsx
@@ -1,5 +1,13 @@
 import { css, type CSSAttribute } from 'goober'
-import { Component, ComponentProps, createMemo, JSX, splitProps, ValidComponent } from 'solid-js'
+import {
+  Component,
+  ComponentProps,
+  createMemo,
+  JSX,
+  mergeProps,
+  splitProps,
+  ValidComponent,
+} from 'solid-js'
 import { Dynamic } from 'solid-js/web'
 
 import { DefaultTheme, useTheme } from './theme'
@@ -26,28 +34,18 @@ const makeStyled = <T extends ValidComponent>(tag: T, g = 0): MakeStyle<T> => {
     (props) => {
       const [local, others] = splitProps(props, ['as', 'class'])
       const theme = useTheme()
+      const withTheme = mergeProps(props, { theme })
       const className = createMemo(() => {
         const c = local.class,
           o = local.class && /^go[0-9]+/.test(c)
-        const p = { ...props, theme }
-        return [
-          local.class,
-          css.apply({ o, g }, [
-            typeof styles === 'function' ? styles(p) : styles,
-            ...args.map((f) => f(p)),
-          ]),
-        ]
-          .filter(Boolean)
-          .join(' ')
+        const s = typeof styles === 'function' ? styles(withTheme as any) : styles
+        const p = args.map((f) => (typeof f === 'function' ? f(withTheme as any) : f))
+        return [local.class, css.apply({ o, g }, [s, ...p])].filter(Boolean).join(' ')
       })
-      const t = createMemo(() => local.as || tag)
       return (
         <Dynamic
-          component={t()}
-          {...({
-            ...others,
-            class: className(),
-          } as any)}
+          component={(local.as || tag) as ValidComponent}
+          {...(mergeProps(others, { class: className() }) as any)}
         />
       )
     }

37571fa0d57851521dd5bf9fa0bc372fac0c9639
ddPn08 <pyn.g.1125@gmail.com>
2022-12-26 20:14:26 +0900

    small fix

--- src/styled.tsx
+++ src/styled.tsx
@@ -1,5 +1,4 @@
-import type { Properties as CSSProperties } from 'csstype'
-import { css } from 'goober'
+import { css, type CSSAttribute } from 'goober'
 import { Component, ComponentProps, createMemo, JSX, splitProps, ValidComponent } from 'solid-js'
 import { Dynamic } from 'solid-js/web'
 
@@ -7,6 +6,7 @@ import { DefaultTheme, useTheme } from './theme'
 
 type PropsType<T extends ValidComponent, P> = ComponentProps<T> & {
   class?: string
+  as?: ValidComponent
 } & P
 
 type StyledPlaceholder<T extends ValidComponent, P = {}> = (
@@ -16,31 +16,34 @@ type StyledPlaceholder<T extends ValidComponent, P = {}> = (
 type MakeStyle<T extends ValidComponent> = <P>(
   styles:
     | TemplateStringsArray
-    | CSSProperties
-    | ((props: PropsType<T, P> & { theme: DefaultTheme }) => string | CSSProperties),
+    | CSSAttribute
+    | ((props: PropsType<T, P> & { theme: DefaultTheme }) => string | CSSAttribute),
   ...args: StyledPlaceholder<T, P>[]
 ) => Component<PropsType<T, P>>
 
-const makeStyled = <T extends ValidComponent>(tag: T): MakeStyle<T> => {
+const makeStyled = <T extends ValidComponent>(tag: T, g = 0): MakeStyle<T> => {
   return (styles, ...args) =>
     (props) => {
-      const [local, others] = splitProps(props, ['class'])
+      const [local, others] = splitProps(props, ['as', 'class'])
       const theme = useTheme()
       const className = createMemo(() => {
-        const style =
-          typeof styles === 'function'
-            ? styles({ ...props, theme })
-            : Array.isArray(styles)
-            ? styles.reduce((a, b, i) => {
-                const fn = args[i - 1]
-                return a + fn?.({ ...props, theme }) + b
-              })
-            : styles
-        return local.class ? `${local.class} ` : '' + css(style)
+        const c = local.class,
+          o = local.class && /^go[0-9]+/.test(c)
+        const p = { ...props, theme }
+        return [
+          local.class,
+          css.apply({ o, g }, [
+            typeof styles === 'function' ? styles(p) : styles,
+            ...args.map((f) => f(p)),
+          ]),
+        ]
+          .filter(Boolean)
+          .join(' ')
       })
+      const t = createMemo(() => local.as || tag)
       return (
         <Dynamic
-          component={tag}
+          component={t()}
           {...({
             ...others,
             class: className(),
